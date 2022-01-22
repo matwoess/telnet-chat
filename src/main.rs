e9772769eb9b199f93991b77c2205ab4bff7fe24
Mathias <mat.woess@gmail.com>
2022-01-22 15:11:00 +0100

    create shared state; struct for user objects; move username prompt out of loop; implement broadcast logic; misc improvements

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::collections::{LinkedList};
+use std::sync::Arc;
+
 use tokio::{
     io::{self, AsyncWriteExt},
     net::TcpListener,
+    net::TcpStream,
+    sync::{broadcast, broadcast::{Receiver, Sender}, Mutex},
 };
-use tokio::net::TcpStream;
 
 use crate::error::CommandError;
 
@@ -11,55 +15,113 @@ mod error;
 const SERVER: &str = "localhost";
 const PORT: &str = "8001";
 
+#[derive(Debug)]
+struct State {
+    server_tx: Sender<String>,
+    //users: HashMap<String, User>,
+    messages: LinkedList<String>,
+}
+
+impl State {
+    fn new(tx: Sender<String>) -> Self {
+        Self { server_tx: tx, /*users: HashMap::new(),*/ messages: LinkedList::new() }
+    }
+
+    fn log_in(&mut self, username: String) -> User {
+        let client_tx = self.server_tx.clone();
+        let client_rx = self.server_tx.subscribe();
+        let user = User::new(client_tx, client_rx, username.clone());
+        //self.users.insert(username, user);
+        return user;
+    }
+}
+
+#[derive(Debug)]
+struct User {
+    name: String,
+    tx: Sender<String>,
+    rx: Receiver<String>,
+}
+
+impl User {
+    fn new(tx: Sender<String>, rx: Receiver<String>, name: String) -> Self {
+        Self { name, tx, rx }
+    }
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let address = format!("{}:{}", SERVER, PORT);
     let listener = TcpListener::bind(address).await?;
-
+    let (tx, rx) = broadcast::channel(16);
+    let state = Arc::new(Mutex::new(State::new(tx)));
+    tokio::spawn(async move {
+        server_receiver(rx).await
+    });
     loop {
         let (socket, addr) = listener.accept().await?;
         println!("Listening to {}", addr);
+        let state = state.clone();
         tokio::spawn(async move {
-            handle_connection(socket).await
+            handle_connection(socket, state).await
         });
     }
 }
 
-async fn handle_connection(mut socket: TcpStream) -> io::Result<()> {
-    let mut username = String::from("");
+async fn server_receiver(mut receiver: Receiver<String>) {
     loop {
-        if username.is_empty() {
-            send_msg(&mut socket, "Please enter your name: ").await?;
-        } else {
-            send_msg(&mut socket, format!("[{}] > ", username).as_str()).await?;
+        let msg = receiver.recv().await;
+        println!("got message '{}'", msg.unwrap());
+    }
+}
+
+async fn handle_connection(mut socket: TcpStream<>, state: Arc<Mutex<State>>) -> io::Result<()> {
+    send_message(&mut socket, "Please enter your name: ").await?;
+    let username = match get_message(&mut socket).await {
+        Ok(name) => name,
+        Err(e) => {
+            println!("error getting username: {}", e);
+            return Ok(());
         }
-        let result = get_message(&mut socket).await;
-        match result {
-            Ok(msg) => {
-                if msg == "quit" {
-                    socket.shutdown().await?;
-                    break;
-                } else {
-                    if username.is_empty() {
-                        username = msg;
-                    } else {
-                        add_chat_message()
+    };
+    let mut user = state.lock().await.log_in(format!("{}", username));
+    loop {
+        send_message(&mut socket, format!("[{}] > ", username).as_str()).await?;
+        tokio::select! {
+        msg = user.rx.recv() => {
+                match msg {
+                    Ok(msg) => {
+                        send_message(&mut socket, format!("\r[usr]: {}\r\n", msg).as_str()).await?;
+                    }
+                    Err(e) => {
+                        eprintln!("error: {:?}", e)
                     }
                 }
             }
-            Err(e) => {
-                eprintln!("{:?}", e);
+            msg = get_message(&mut socket) => {
+                match msg {
+                    Ok(msg) => {
+                        if msg == "quit" {
+                            socket.shutdown().await?;
+                            break;
+                        } else {
+                            match user.tx.send(msg) {
+                                Ok(_) => {},
+                                Err(e) => println!("error while sending: {}", e),
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("{:?}", e);
+                    }
+                };
             }
-        };
+        }
     }
     Ok(())
 }
 
-fn add_chat_message() {
-    todo!()
-}
-
-async fn send_msg(socket: &mut TcpStream, msg: &str) -> io::Result<()> {
+async fn send_message(socket: &mut TcpStream, msg: &str) -> io::Result<()> {
     socket.write_all(msg.as_bytes()).await
 }
 
